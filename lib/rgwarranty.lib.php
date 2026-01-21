53db08518b907ef0dea443f704495ec434403cd8
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2026-01-21 21:41:23 +0100

    Update rgwarranty.lib.php

--- lib/rgwarranty.lib.php
+++ lib/rgwarranty.lib.php
@@ -151,7 +151,7 @@ function rgwarranty_fetch_invoices_for_cycle($db, $entity, $situationCycleRef)
 	$invoices = array();
 	// EN: Use fk_statut field name for invoices
 	// FR: Utiliser le champ fk_statut pour les factures
-	$sql = "SELECT f.rowid, f.ref, f.datef, f.total_ttc, f.fk_statut as status, f.retained_warranty, f.multicurrency_total_ttc, f.multicurrency_code, f.fk_soc, f.fk_projet";
+	$sql = "SELECT f.rowid, f.ref, f.datef, f.total_ttc, f.fk_statut, f.retained_warranty, f.multicurrency_total_ttc, f.multicurrency_code, f.fk_soc, f.fk_projet";
 	$sql .= " FROM ".$db->prefix()."facture as f";
 	$sql .= " WHERE f.entity = ".((int) $entity);
 	$sql .= " AND f.situation_cycle_ref = ".((int) $situationCycleRef);
