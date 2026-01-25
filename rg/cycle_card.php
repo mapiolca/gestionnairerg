37e8295e99360a738c2673899a7330312b24bdc2
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2026-01-25 21:09:09 +0100

    Fix header display in cycle_card.php

--- rg/cycle_card.php
+++ rg/cycle_card.php
@@ -190,9 +190,7 @@ $totals = rgwarranty_get_cycle_totals($db, $object->id);
 llxHeader('', $langs->trans('RGWCycle'));
 
 $head = rgwarranty_cycle_prepare_head($object);
-//print dol_get_fiche_head($head, 'card', $langs->trans('RGWCycle'), -1, 'invoicing');
-print dol_get_fiche_head($head, 'card', $langs->trans('RGWCycle'), -1, $object->picto());
-
+print dol_get_fiche_head($head, 'card', $langs->trans('RGWCycle'), -1, 'invoicing');
 // EN: Entity scope (multicompany)
 // FR: Périmètre entity (multicompany)
 $entity = (!empty($object->entity) ? (int) $object->entity : (int) $conf->entity);
